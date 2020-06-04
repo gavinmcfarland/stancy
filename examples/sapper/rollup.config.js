9829755e9fe8f4adf30b183ebc3f8244ceadeb70
limitlessloop <5551+limitlessloop@users.noreply.github.com>
2020-06-04 11:45:49 +0100

    Updated examples

--- examples/sapper/rollup.config.js
+++ examples/sapper/rollup.config.js
@@ -6,7 +6,7 @@ import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 import config from 'sapper/config/rollup.js';
 import pkg from './package.json';
-import json from 'rollup-plugin-json';
+import json from '@rollup/plugin-json';
 
 const mode = process.env.NODE_ENV;
 const dev = mode === 'development';
