5b7c343eff59261064e788adbbd884e6619219b3
limitlessloop <5551+limitlessloop@users.noreply.github.com>
2020-06-25 22:54:29 +0100

    Updated website

--- website/src/components/Nav.svelte
+++ website/src/components/Nav.svelte
@@ -14,8 +14,8 @@
 	<div class="wrapper" flex column-gap="1">
 		<a aria-current={segment === undefined ? 'page' : undefined} href=".">
 			<svg
-				width="32"
-				height="32"
+				width="24"
+				height="24"
 				viewBox="0 0 32 32"
 				fill="none"
 				xmlns="http://www.w3.org/2000/svg">
