6c327d8ba6fad4a418c203cf5acfa009d49d64a4
limitlessloop <5551+limitlessloop@users.noreply.github.com>
2020-06-26 10:38:59 +0100

    Updated website

--- website/stancyClient.js
+++ website/stancyClient.js
@@ -1,7 +1,15 @@
 import stancy from 'stancy';
 import marked from 'marked';
+import hljs from 'highlight.js';
 
 const client = stancy('content/').client('https://stancy.vercel.app/api/');
+
+marked.setOptions({
+	highlight: function (code, language) {
+		const validLanguage = hljs.getLanguage(language) ? language : 'plaintext';
+		return hljs.highlight(validLanguage, code).value;
+	},
+});
 
 client.preprocess('content', (data) => {
 	return marked(data);
